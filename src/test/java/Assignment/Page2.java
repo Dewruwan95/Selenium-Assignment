62885ed377dd5d3d40324fb504e5a60dce2e18c8
Dewruwan95 <dewruwanebay@gmail.com>
2020-11-16 12:20:26 +0530

    Create page 2

--- src/test/java/Assignment/Page2.java
+++ src/test/java/Assignment/Page2.java
@@ -2,7 +2,6 @@ package Assignment;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 public class Page2 extends Page1 {
--- src/test/java/Assignment/Page3.java
+++ src/test/java/Assignment/Page3.java
@@ -3,7 +3,6 @@ package Assignment;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 public class Page3 extends Page2 {
@@ -11,12 +10,8 @@ public class Page3 extends Page2 {
     //8. Select a size
     @Test(priority = 7)
     public void selectProductSize() {
-        System.out.println("before select");
-
-        Select sizeDropDownSelect = new Select(driver.findElement(By.xpath("//body/div[@id='a-page']/div[@id='dp']/div[@id='dp-container']/div[@id='ppd']/div[2]/div[2]/div[1]/div[1]/div[1]/div[12]/div[1]/div[2]/form[1]/div[2]/span[1]/span[1]/span[1]/span[1]/span[1]")));
-        System.out.println("after selected");
-        sizeDropDownSelect.selectByValue("2,B0785VMVF4");
-
+        Select shirtSize = new Select(driver.findElement(By.id("native_dropdown_selected_size_name")));
+        shirtSize.selectByValue("2,B0785VMVF4");
     }
 
 //    @AfterTest
